f1600f17878a37192a70bdf5b13055028180a460
e2400096 <chloesondag.pro@gmail.com>
2025-02-11 16:28:18 +0100

    ll

--- TD1_arguments.py
+++ TD1_arguments.py
@@ -1,14 +1,23 @@
 import sys
+from pathlib import Path
 
+
+
+print("bonjour")
 if __name__ == '__main__':
+
     print(sys.argv[0])
 
-    if len(sys.argv) == 1 :
-        print('Aucun argument après le nom du script : ', sys.argv[0])
+    if len(sys.argv) < 2 :
+        print(f'Aucun argument après le nom du script : {sys.argv[0]}')
+    else :
+        for i in range(1, len(sys.argv)) :
+            print(f'\t--> {sys.argv[i]}')
 
-    for i in range(len(sys.argv)): # Boucle avec index…
-        print(sys.argv[i])
-###
+
+##    for i in range(len(sys.argv)): # Boucle avec index…
+#      print(sys.argv[i])
+
 #    for argument in sys.argv: # Boucle avec élément de la liste…
 #        print(argument)
-###
+
---
+++ ex2.py
@@ -0,0 +1,1 @@
+import TD1_arguments
