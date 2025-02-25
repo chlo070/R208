220cf8aa9a6500653d66fe2b5dfa00d785b99150
e2400096 <chloesondag.pro@gmail.com>
2025-02-25 11:48:35 +0100

    lll

---
+++ TD2/(2.2)ip_log.py
@@ -0,0 +1,7 @@
+#objectif : chercher dans un fichier « log » le nombre d’occurrence d’une adresse IP
+#syntaxe --> python ip_log.py nom_fichier.csv -ip adresse_ip
+
+
+#correction
+
+#fin correction
