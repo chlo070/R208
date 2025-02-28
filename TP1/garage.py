00a54b55035294228391af5e603b70672049106e
e2400096 <chloesondag.pro@gmail.com>
2025-02-28 11:57:41 +0100

    cdsvd

--- TP1/garage.py
+++ TP1/garage.py
@@ -1,9 +1,18 @@
 #main exo 2
 from obj_voiture import Voitures
 
-carA = Voitures("Renault", "Captur_TCE_90ch",2018, 20000, "Gris foncé", 7.2)  # Création d’une instance.
+# Création d’une instance.
+carA = Voitures("Renault", "Captur_TCE_90ch",2018, 20000, "Gris foncé", 7.2)
 caisse = carA.modele  # Lecture d’un attribut d’instance.
 print(carA)  # Affichage.
-carB = Voitures("Renault", "Clio_TCE_100ch",2018, 17000, "Bleu nuit", 5.5)  # Création d’une instance.
+# Création d’une 2e instance.
+carB = Voitures("Renault", "Clio_TCE_100ch",2018, 17000, "Bleu nuit", 5.5)
 caisse = carB.modele  # Lecture d’un attribut d’instance.
 print(carB)  # Affichage.
+# Test de la méthode calcul_consommation
+print("Consommation :", carA.calcul_consommation(int(input("Quelle est la distance parcourue?"))))
+print("Consommation :", carB.calcul_consommation(int(input("Quelle est la distance parcourue?"))))
+# Test de la méthode calcul_prix
+print("Prix :", carA.calcul_prix(1000))
+print("Prix :", carB.calcul_prix(1000))
+
--- TP1/obj_voiture.py
+++ TP1/obj_voiture.py
@@ -1,6 +1,7 @@
 #module exo 2
 #class
 class Voitures():
+    prix_litre = 1.70
     # Constructeur avec 3 arguments...
     def __init__(self, marque="Ferrari", modele="SF90_spider", annee=2010, prix=None, couleur="Blanc", conso=6.0) :
         # Trois attributs d’instance...
@@ -11,8 +12,16 @@ class Voitures():
         self.couleur = couleur
         self.conso = conso
 
-
     def __str__(self):
         # Redéfinition pour le print(instance)...
         return f"Voiture : {self.marque} {self.modele} - {self.annee} - {self.couleur} - {self.conso}L/100km - {self.prix}€"
+    # Calcul du nombre de litre de carburant utilisé pour faire une distance
+    def calcul_consommation(self, distance) :
+        return self.conso * (distance/100)
+    def calcul_prix(self, distance) :
+        return Voitures.prix_litre * self.calcul_consommation(distance)
+
+    def modif_prix_litre(self, new_prix_litre) :
+        Voitures.prix_litre = new_prix_litre
+
 
