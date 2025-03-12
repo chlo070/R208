8907b52fb61de9356c9a2237a1fbe6fa56b42ad9
e2400096 <chloesondag.pro@gmail.com>
2025-03-12 15:15:53 +0100

    fb

--- TP2/obj_livre.py
+++ TP2/obj_livre.py
@@ -7,10 +7,10 @@ class Livre(Couleur):
         Livre.nombre_total_livres += 1    # Incrémentation de l'instance de classe
         # Six attributs d’instance
         self.id = Livre.nombre_total_livres
-        self.titre = nom.upper()
-        self.auteur = prenom.capitalize()
-        self.isbn = pays
-        self.annee_publication = annee_publication
+        self.titre = titre
+        self.auteur = auteur
+        self.isbn = isbn if isbn else "N/A"
+        self.annee_publication = annee_publication if annee_publication else "inconnue"
         if pays == None :
             self.pays = "inconnu"
         if date_naissance == None :
@@ -19,3 +19,13 @@ class Livre(Couleur):
         return f"{Auteur.BLEU}{self.id} {Auteur.NO_COLOR} {self.nom} {self.prenom} {Auteur.MAGENTA}(né(e) le {self.date_naissance} en {self.pays}) {Auteur.NO_COLOR}"
 
 
+follett = Auteur("FOLLETT", "Ken", "Pays de Galles", "05/06/1949")
+verne = Auteur("VERNE","Jules","France", "08/02/1828")
+print("Création de 3 instances de Livre et affichage...")
+livre_1 = Livre("Les Piliers de la Terre", follett, "9782130428114", "1989")
+livre_2 = Livre("Une Colonne de Feu", follett, "9782221157695", "2017")
+livre_2.disponible = False
+livre_3 = Livre("Vingt Mille Lieues sous les mers", verne, "9782070364234", "1870")
+print(livre_1)
+print(livre_2)
+print(livre_3)
