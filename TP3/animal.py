b462077d296ec3617fff0ff797e5ea08b2401cec
chlo070 <chloesondag.pro@gmail.com>
2025-03-18 08:08:56 +0100

    cgn

--- TP3/animal.py
+++ TP3/animal.py
@@ -1,9 +1,10 @@
 
-class Parent:
+class Animal :
     def se_presenter(self, nom, age) :   # Constructeur et ses 2 arguments
         # Deux attributs d’instance / instanciation
         self.nom = nom
         self.age = age
+        print(f"Je me nomme {nom}, j’ai {age} ans…")
 
 animaux = [Animal("Simba", 5), Animal("Beethoven", 3), Animal("César", 26), Animal("Dumbo", 1)]
 
@@ -11,5 +12,5 @@ for animal in  animaux:
     animal.se_presenter()
 
 
-class Mammifère:
-def afficher(self):
+#class Mammifère:
+ #   def afficher(self):
